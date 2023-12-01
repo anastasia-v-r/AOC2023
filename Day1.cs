a34212c0881121e15b72a844ee8c3bc174bfd152
Dominik Rutter <anastasia.v.rutter@gmail.com>
2023-11-30 22:04:34 -0500

    Add project files.

---
+++ Day1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOC2023
+{
+    internal class Day1
+    {
+        static void Run()
+        {
+            Console.WriteLine("Day 1 project!");
+        }
+    }
+}
